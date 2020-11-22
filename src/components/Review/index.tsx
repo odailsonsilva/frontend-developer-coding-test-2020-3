df78850f6c444f5e33af95befb4deefcd1e2fafc
odailsonsilva <odafreelancerti@gmail.com>
2020-11-22 19:01:46 -0300

    fix: git

---
+++ src/components/Review/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Review: React.FC = () => {
+  return (
+    <Container>
+      <h1>Review</h1>
+    </Container>
+  );
+};
+
+export default Review;
