eb2e9063a37fd64fe0917c5017ad5919f53816b0
odailsonsilva <odafreelancerti@gmail.com>
2020-11-22 18:42:51 -0300

    feat: styles component categories

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { MdKeyboardArrowDown } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, ListDrop } from './styles';
 
-const Categories: React.FC = () => {
+const Price: React.FC = () => {
+  const [listDrop, setListDrop] = useState(false);
+
+  const handleDrop = useCallback(() => {
+    if (listDrop === false) {
+      setListDrop(true);
+    } else {
+      setListDrop(false);
+    }
+  }, [listDrop]);
+
   return (
-    <Container>
-      <h1>Categories</h1>
+    <Container isDrop={listDrop} onClick={handleDrop}>
+      <ul>
+        <li>
+          Categories
+          <ListDrop isDrop={listDrop}>
+            <li>
+              <input type="radio" id="all2" name="categorie" />
+              <label htmlFor="all2">All</label>
+            </li>
+            <li>
+              <input type="radio" id="ita" name="categorie" />
+              <label htmlFor="ita">Italian</label>
+            </li>
+            <li>
+              <input type="radio" id="sea" name="categorie" />
+              <label htmlFor="sea">Seafood</label>
+            </li>
+            <li>
+              <input type="radio" id="steak" name="categorie" />
+              <label htmlFor="steak">Steakhouses</label>
+            </li>
+            <li>
+              <input type="radio" id="japa" name="categorie" />
+              <label htmlFor="japa">Japanese</label>
+            </li>
+            <li>
+              <input type="radio" id="ame" name="categorie" />
+              <label htmlFor="ame">American</label>
+            </li>
+            <li>
+              <input type="radio" id="mex" name="categorie" />
+              <label htmlFor="mex">Mexican</label>
+            </li>
+            <li>
+              <input type="radio" id="thai" name="categorie" />
+              <label htmlFor="thai">Thai</label>
+            </li>
+          </ListDrop>
+        </li>
+      </ul>
+      <span>
+        <MdKeyboardArrowDown />
+      </span>
     </Container>
   );
 };
 
-export default Categories;
+export default Price;
